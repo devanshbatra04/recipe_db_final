1a6f5d4d1bd2536f77bfb32dd85f5318b54ee2a7
Utkarsh Upadhyay <utkarshdhy@gmail.com>
2019-06-29 20:35:31 +0530

    Howto.html done.

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def get_bold_num(page, page_num_list):
 
 #function to query for Ingredients
 def exec_ingre_query():
-	# print("lol")
+	print("lol")
 
 def exec_query(name, region, Sub_region, page,ings,not_ings, category, not_category, recipe_ids,include_nutrBorders=None, dict_nut_boundaries={}):
 	limit = " LIMIT 20 OFFSET " + str(((int(page)-1) * 20))
@@ -226,9 +226,16 @@ def load_conceptMap():
 def load_Ingstats():
 	return render_template('recipe_size.html')
 
-@app.route('/recipedb/')
+@app.route('/recipedb/', methods=['GET', 'POST'])
 def home():
-	return render_template('home.html', empty = "no")
+	con = sql.connect("recipe2-final.db")
+	con.row_factory = sql.Row
+	query='SELECT * FROM recipes2 ORDER BY RANDOM() LIMIT 1'
+	cur = con.cursor()
+	cur.execute(query)
+	row=cur.fetchall()
+	print(row[0]['Recipe_title'])
+	return render_template('home.html', empty = "no",row=row)
 
 @app.route('/recipedb/all_recipes', methods=['GET'])
 def all_recipes():
